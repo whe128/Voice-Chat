9a1944ca197c1034797a5f8a13a5f2191e602c11
whe128 <whe545@outlook.com>
2025-10-26 20:05:43 +1100

    fix: add warmup request handling in AccessKeyMiddleware

--- backend/Middleware/AccessKeyMiddleware.cs
+++ backend/Middleware/AccessKeyMiddleware.cs
@@ -14,6 +14,13 @@ public class AccessKeyMiddleware
     public async Task InvokeAsync(HttpContext context)
     {
 
+        var path = context.Request.Path.Value;
+        if (context.Request.Headers["X-WEBSITE-WARMUP"].Count != 0 && path == "/")
+        {
+            await _next(context);
+            return;
+        }
+
         if (!context.Request.Headers.TryGetValue("accessKey", out var extractedAccessKey) ||
             extractedAccessKey != _accessKey)
         {
