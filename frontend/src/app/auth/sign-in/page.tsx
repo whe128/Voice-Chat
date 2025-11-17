f9025f91f7019f93cd6f6778b4d9be3ca23402e7
whe128 <whe545@outlook.com>
2025-11-17 23:15:47 +1100

    refactor: simplify SignOut component to use implicit return

--- frontend/src/app/auth/sign-in/page.tsx
+++ frontend/src/app/auth/sign-in/page.tsx
@@ -3,14 +3,10 @@
 import { FC } from 'react';
 import LoginForm from '@/components/LoginForm';
 
-const SignOut: FC = () => {
-  const a = 1;
-
-  return (
-    <div className="flex h-dvh w-full max-w-lg mx-auto justify-center items-center">
-      <LoginForm />
-    </div>
-  );
-};
+const SignOut: FC = () => (
+  <div className="flex h-dvh w-full max-w-lg mx-auto justify-center items-center">
+    <LoginForm />
+  </div>
+);
 
 export default SignOut;
